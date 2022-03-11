a43b9ec21740fb152359b1e58233119d67b1f319
Hank <hz4154261@gmail.com>
2022-03-11 13:31:27 +0800

    fix

--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
-from ntpath import join
-from xml.dom.pulldom import CHARACTERS
 from NetPacketTools.packet_action import PacketAction
 from NetPacketTools.packet_listen import PacketListenFromFilter
-import re
 
 lan1_ = PacketAction()
 mac = lan1_.GetIPv6MAC('fe80::250:56ff:fe67:9d8f')
