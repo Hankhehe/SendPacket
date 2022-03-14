aa1a6f08ae0e145fc73c87a3c705c9f29d8df4dd
Hank <hz4154261@gmail.com>
2022-03-14 12:54:01 +0800

    fix

--- Main.py
+++ Main.py
@@ -2,9 +2,15 @@ from NetPacketTools.packet_action import PacketAction
 from NetPacketTools.packet_listen import PacketListenFromFilter
 from CreateData import iprelated,macrelated
 
-ipv4data = iprelated.CreateIPDataByCIDROrPrifix('192.168.11.1/24')
-ipv6data = iprelated.CreateIPDataByCIDROrPrifix('2001:b030:2133:80b::/112')
+ipv4data = iprelated.CreateIPDataByCIDROrPrifix('192.168.11.1/22')
+ipv6data = iprelated.CreateIPDataByCIDROrPrifix('2001:b030:2133:80b:192:168:11:1/108')
+macdata = macrelated.CreateMACData('AA0000000000',1024)
 
+if ipv4data and ipv6data and macdata:
+    for i in range(1,1023):
+        with open('output.csv','a') as file:
+            file.write(f'{ipv4data[i]},{ipv6data[i]},{macdata[i]} \n')
+pass
 if ipv6data : 
     for i in ipv6data:
         print (iprelated.ConvertIPv6ShortToIPv6Full(str(i)))
