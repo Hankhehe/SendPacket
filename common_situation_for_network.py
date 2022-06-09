c6c8746ab515531decfedcf4474acb91225d6fbe
Hank <hz4154261@gmail.com>
2022-06-09 15:47:52 +0800

    fix

--- common_situation_for_network.py
+++ common_situation_for_network.py
@@ -1,14 +1,26 @@
+from string import punctuation
 from NetPacketTools.packet_action import PacketAction
 from NetPacketTools.packet_listen import PacketListenFromFilter
 from NetPacketTools.packet_action_test import PacketRelated8021X
 from CreateData import iprelated,macrelated
 import time,csv
 
-def SendIPconflict(ip:str) -> None:
+def SendIPconflict(ip:str,mac1:str,mac2:str,count:int) -> None:
     '''發送 IP 衝突'''
-    while True :
-        lan1.SendARPReply(IP=ip,MAC='aa:00:00:00:00:00')
-        lan1.SendARPReply(IP=ip,MAC='aa:00:00:00:00:01')
+    MACAddress1 = macrelated.ConvertMACbyPunctuation(mac=mac1,Punctuation=':')
+    MACAddress2 = macrelated.ConvertMACbyPunctuation(mac=mac2,Punctuation=':')
+    for i in range(count):
+        lan1.SendARPReply(IP=ip,MAC=MACAddress1)
+        lan1.SendARPReply(IP=ip,MAC=MACAddress2)
+        time.sleep(1)
+
+def SendIPv6Conflict(ipv6:str,mac1:str,mac2:str,count:int) -> None:
+    '''發送 IPv6 衝突'''
+    MACAddress1 = macrelated.ConvertMACbyPunctuation(mac=mac1,Punctuation=':')
+    MACAddress2 = macrelated.ConvertMACbyPunctuation(mac=mac2,Punctuation=':')
+    for i in range(count):
+        lan1.SendNA(IP=ipv6,MAC=MACAddress1)
+        lan1.SendNA(IP=ipv6,MAC=MACAddress2)
         time.sleep(1)
 
 def SendManyBroadcastOfARP() -> None:
@@ -17,5 +29,6 @@ def SendManyBroadcastOfARP() -> None:
         lan1.SendARPReply(IP=lan1.Ip)
 
 lan1 = PacketAction('乙太網路')
-# SendIPconflict(ip='192.168.11.32')
+# SendIPconflict(ip='192.168.11.32',mac1='aa0000000000',mac2='aa0000000001',count=3)
+# SendIPv6Conflict(ipv6='2001:b030:2133:80b::11:32',mac1='aa0000000000',mac2='aa0000000001',count=3)
 # SendManyBroadcastOfARP()
